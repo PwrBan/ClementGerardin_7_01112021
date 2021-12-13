c048169d398e80f6267946baa2756f24d5230813
Clement Gerardin <clementgerardin.cg@gmail.com>
2021-12-13 09:39:27 +0100

    Update auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,9 +1,11 @@
 const jwt = require('jsonwebtoken');
+const fs = require('fs');
+const tokenKey = fs.readFileSync('./environment/private-token.key')
 
 module.exports = (req, res, next) => { 
     try {
         const token = req.headers.authorization.split(' ')[1];
-        const decodedToken = jwt.verify(token, 'SECRET_KEY');
+        const decodedToken = jwt.verify(token, tokenKey);
         const userId = decodedToken.userId;
         const isAdmin = decodedToken.admin;
         if (req.body.userId && req.body.userId !== userId || isAdmin === 0) {
